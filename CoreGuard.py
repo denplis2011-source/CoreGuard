df6257a20d57606d780838a9e706451e1e0b5245
denplis2011-source <denplis2011@gmail.com>
2026-09-07 21:45:31 +0200

    Update CoreGuard.py

--- CoreGuard.py
+++ CoreGuard.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 )
 
 # ================== CONFIGURATION ==================
-TOKEN = "8885507527:AAGMTT4NT_OGXgbwr5CATf45amLKa5wwUYM"                    # ← Remplace par ton vrai token
+TOKEN = "TOKEN"                    # ← Remplace par ton vrai token
 OPENAI_API_KEY = ""                        # laisse vide pour l’instant
 MAX_WARNINGS = 3
 MUTE_DURATION = 3600
